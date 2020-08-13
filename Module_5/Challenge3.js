17c8d007dc81f0c960c9fc82f1c97e0d8c248461
Tinesha N. Conway <66802899+nicolette663@users.noreply.github.com>
2020-08-13 03:03:18 -0400

    Update Challenge3.js

--- Module_5/Challenge3.js
+++ Module_5/Challenge3.js
@@ -1,9 +1,14 @@
 const lyrics = [`I messed up tonight, I lost another fight, I messed up but I'll just start again, I keep falling down I keep on hitting the ground, I always get up now to see what's next. Birds don't just fly, they fall down and get up.  Nobody learns without getting it wrong! I won't give up, no I won't give in, till I reach the end and then I'll start again. No I won't leave, I wanna try everything, I wanna try even though I could fail!`];
 
-const words = lyrics.split(" ").join();
+let words = lyrics.split(" ")
 
-array.splice("s", '$');
+for (let i=0; i < lyrics.length; i++) {
+  if (words[i] == "e" && words[i] == "E") {
+   let newLyrics = words[i].replace(/e/ig,  "$");
+  }
+}
 
-console.log(lyrics);
+Let newLyrics = newString.join(" ");
 
+console.log(newLyrics);
 
