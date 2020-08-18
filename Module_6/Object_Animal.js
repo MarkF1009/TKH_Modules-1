08465fefa349d233d93de32716baf9fea2ff1486
Tinesha N. Conway <66802899+nicolette663@users.noreply.github.com>
2020-08-17 23:39:28 -0400

    Add files via upload

---
+++ Module_6/Object_Animal.js
@@ -0,0 +1,23 @@
+/*Create an Animal*/
+
+let animal = {
+	name: "furballs",
+	color: "white", 
+	breed: "bear",
+	softness: "sinkably soft",
+	makes_noise: function(){
+		console.log ("meow");
+		return true
+	},
+		
+		get name( ) {
+			return this.animalName;
+	},
+		set breed(breed) {
+			this.breed = animalBreed;
+	}
+};
+
+animal.name
+animal.breed
+animal.makes_noise
